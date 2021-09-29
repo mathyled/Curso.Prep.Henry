675fa6a3e013e3b3c8f6e05de32d99f33448e891
unknown <mathiasimagine@gmail.com>
2021-09-29 20:38:19 -0300

    Subo otra parte de ejercicio 2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -122,6 +122,11 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+if ( num1 < 0 || num2 < 0 || num3 < 0 ) { return "Hay negativos"}
+else if ( num1 === 0 || num2 === 0 || num3 === 0) { return "Error"}
+  else if ( num1 > num2 && num1 > num3 && num1 > 0 ) { return "Número 1 es mayor y positivo"}
+else if ( num3 > num1 && num3 > num2 ) { return num3 + 1 }
+else { return false;}
 }
 
 function esPrimo(numero) {
@@ -130,6 +135,13 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  //
+  if ( numero < 2 ) { return false};
+  if ( numero === 2 ) { return true };
+for ( let i = 2 ; i < numero ; i++ ) {
+  if ( numero % i === 0 ) { return false;}
+}
+return true;
 }
 
 function esVerdadero(valor){
